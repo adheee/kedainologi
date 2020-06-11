37ec606b3cd806c03af4b683c29573398548d289
adheee <adheejr77@gmail.com>
2020-06-11 13:34:17 +0800

    add active navbar

--- js/style.js
+++ js/style.js
@@ -1,11 +1,30 @@
+$(document).ready(function () {
+
+	$('.page-scroll').on("click", function (e) {
+		var anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $(anchor.attr('href')).offset().top - 100
+		}, 1200);
+		e.preventDefault();
+	});
+});
+
+// active menu
+$(document).ready(function(){
+	$('ul li .page-scroll').click(function(){
+		$('li .page-scroll').removeClass("active");
+		$(this).addClass("active");
+	});
+});
+
 // navbar scroll
 $(window).scroll(function () {
-    var scroll = $(window).scrollTop();
+	var scroll = $(window).scrollTop();
 
-    if (scroll >= 50) {
-        $("nav").addClass("navbar-scroll");
-    } else {
-        $("nav").removeClass("navbar-scroll");
-    }
+	if (scroll >= 50) {
+		$("nav").addClass("navbar-scroll");
+	} else {
+		$("nav").removeClass("navbar-scroll");
+	}
 });
 // end navbar scroll
