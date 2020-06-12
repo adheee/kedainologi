f7f33438495b4a7d2aa0c2a601a2132be67c98bf
adheee <adheejr77@gmail.com>
2020-06-12 20:08:07 +0800

    navbar collapse done

--- js/style.js
+++ js/style.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 	$('.page-scroll').on("click", function (e) {
 		var anchor = $(this);
 		$('html, body').stop().animate({
-			scrollTop: $(anchor.attr('href')).offset().top - 70
+			scrollTop: $(anchor.attr('href')).offset().top - 20
 		}, 1200);
 		e.preventDefault();
 	});
